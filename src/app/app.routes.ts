dafbc4aaac3bca265bfde912df6b4a14abfcd480
Matheus Coutinho <matheus.coutinho@fit-tecnologia.org.br>
2024-04-14 00:18:04 -0400

    Feat/HttpClient Setup Api Service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  constructor(private readonly productsService: ProductsService) {}
+
+  ngOnInit() {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
+}
